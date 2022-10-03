2d583fb9e4bd3af0766daa02bb9fdd5e77d3205a
Ch <77012163+chiefbrai@users.noreply.github.com>
2022-10-03 20:55:54 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,48 @@
+import time
+
+import random 
+
+import os
+
+print("hello")
+
+time.sleep(1)
+
+os.system('clear')
+
+a = random.randint(1, 10)
+
+b = int(input("pls write a number range 1 10: \n "))
+
+time.sleep(1)
+
+if b == a:
+
+  print("congrulations you win \n")
+
+elif b > 10:
+
+  print("i said range 1,10")
+
+elif b < 0:
+
+  print("why did you not listening me")
+
+else:
+
+  print("you lose the number is", a)
+
+print("\n \n thanks for using.My repl:\n replit.com/@chiefbrain")
+
+time.sleep(10)
+
+i=1
+
+while 1<1000000:
+
+  print("\n \n thanks for using.My repl:\n replit.com/@chiefbrain")
+
+  os.system('clear')
+
+  i+=1
+
